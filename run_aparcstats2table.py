aa3dad6d9e35e4cf452533319d4b4050d9d04271
= <stbrown@mcin.ca>
2018-12-18 08:09:24 -0500

    Now using a file collection

--- run_aparcstats2table.py
+++ run_aparcstats2table.py
@@ -4,26 +4,24 @@ import os
 import glob
 import csv
 
+
+def dir_exists(parser, dir_name):
+    if not os.path.isdir(dir_name):
+        parser.error("Directory not correct, either doesn't exist or is not a directory {0}".format(dir_name))
+    return dir_name
+
+
 def main(args=None):
     parser = argparse.ArgumentParser(description='FreeSurfer aparcstats2table')
-    parser.add_argument('--cbcsv_file',
-       		        help='CBRAIN CBCSV file with  directories that one can use that contains the subject data')
+    parser.add_argument('--filecol',type=lambda x: dir_exists(parser,x), 
+       		        help='CBRAIN file collection with all directories that one can use that contains the subject data')
     parser.add_argument('--subjects_file',
                         help='The output subject file')
     opts = parser.parse_args() if args is None else parser.parse_args(args)
 
     
-    subject_dirs = []
-    with open("{0}".format(opts.cbcsv_file,"r")) as f:
-        csvReader = csv.reader(f)
-        for row in csvReader:
-            print(row[1])
-            sub_dir = glob.glob("{0}/sub-*".format(row[1]))
-            print(sub_dir)
-            if len(sub_dir) > 0:
-                subject_dirs.append(sub_dir[0])
+    subject_dirs = glob.glob("{0}/*/sub-*".format(opts.filecol))
 
-    print(subject_dirs)
     if len(subject_dirs) > 0:
         with open("{0}".format(opts.subjects_file),"w") as f:
             for x in subject_dirs[:-1]:
